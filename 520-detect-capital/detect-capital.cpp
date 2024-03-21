f8351e0acde921727979230562fb1c99c231399c
Prathmesh Gavande <126398500+prathmesh-gavande@users.noreply.github.com>
2024-03-21 22:38:46 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (10.29%) - LeetSync

---
+++ 520-detect-capital/detect-capital.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int countCapitals = 0;
+        
+        for(char &ch : word) {
+            if(isupper(ch))
+                countCapitals++;
+        }
+        
+        if(countCapitals == 0)
+            return true;
+        
+        if(countCapitals == word.length())
+            return true;
+        
+        if(countCapitals == 1 && isupper(word[0]))
+            return true;
+        
+        return false;
+    }
+};
