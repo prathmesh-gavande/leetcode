da4a3d0d5195c75229aa6bad26109464398a32e6
Prathmesh Gavande <126398500+prathmesh-gavande@users.noreply.github.com>
2024-08-31 00:31:17 +0530

    Time: 7 ms (21.64%), Space: 43.2 MB (7.39%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -0,0 +1,19 @@
+class Solution {
+   public String mergeAlternately(String word1, String word2) {
+       
+       String result = "";
+       int len1 = word1.length();
+       int len2 = word2.length();
+       int maxLength = Math.max(len1, len2);
+
+       for (int i = 0; i < maxLength; i++) {
+           if (i < len1) {
+               result += word1.charAt(i);
+           }
+           if (i < len2) {
+               result += word2.charAt(i);
+           }
+       }
+       return result;
+   }
+}
